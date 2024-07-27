6f38b66a9b5f5f8043b5efbfa681a604baafeff8
rikeda-cloud <longsichitian648@gmail.com>
2024-07-28 00:07:54 +0900

    add multi index to node_a_id, node_b_id

--- webapp/mysql/migration/1_rikeda.sql
+++ webapp/mysql/migration/1_rikeda.sql
@@ -4,3 +4,5 @@ CREATE INDEX idx_car_value ON orders(car_value);
 CREATE INDEX idx_status ON orders(status);
 CREATE INDEX idx_order_time ON orders(order_time);
 CREATE INDEX idx_node_id ON orders(node_id);
+
+CREATE INDEX idxs_node_id ON edges(node_a_id, node_b_id);
