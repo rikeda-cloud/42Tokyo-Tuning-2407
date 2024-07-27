1ca4255e525c7e7e3fcc5df47b22098a45d05c79
rikeda-cloud <longsichitian648@gmail.com>
2024-07-27 17:31:37 +0900

    add index to orders table. score:1656

--- webapp/mysql/migration/1_rikeda.sql
+++ webapp/mysql/migration/1_rikeda.sql
@@ -1,1 +1,6 @@
 CREATE INDEX idx_user_id ON dispatchers(user_id);
+
+CREATE INDEX idx_car_value ON orders(car_value);
+CREATE INDEX idx_status ON orders(status);
+CREATE INDEX idx_order_time ON orders(order_time);
+CREATE INDEX idx_node_id ON orders(node_id);
